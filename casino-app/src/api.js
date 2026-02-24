3bb738c7a266acaebfa7b0ce556f0f09c3bfc057
Kimi Schwein <kimi-schwein@web.de>
2026-02-24 20:52:17 +0100

    API URL fuer Produktion

--- casino-app/src/api.js
+++ casino-app/src/api.js
@@ -1,4 +1,4 @@
-const BASE = 'http://localhost:5000/api';
+const BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 const getToken = () => localStorage.getItem('token');
 
 export const api = {
